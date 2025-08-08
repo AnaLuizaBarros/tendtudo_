c1e063121671efb18effcb0a1d4c1e3ed2812197
Ana Luiza <analuizagbarros@gmail.com>
2025-08-08 19:32:05 -0300

    Feat: Header e footer da pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app.routes';
 
 @Component({
   selector: 'tt-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedModule, AppRoutingModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tendtudo';
