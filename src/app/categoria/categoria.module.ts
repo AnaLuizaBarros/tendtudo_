1027d82ea11b2ce04dbd0daa9214deb9526edc21
Ana Luiza <analuizagbarros@gmail.com>
2025-08-11 11:21:37 -0300

    Refact: Inserindo componente de estrelas e preço nos componentes

--- src/app/categoria/categoria.module.ts
+++ src/app/categoria/categoria.module.ts
@@ -4,10 +4,17 @@ import { ListagemComponent } from './listagem/listagem.component';
 import { CommonModule } from '@angular/common';
 import { CategoriaRoutingModule } from './categoria-routing.module';
 import { ProdutoDetalheComponent } from './produto-detalhe/produto-detalhe.component';
+import { ResultadoPesquisaComponent } from './resultado-pesquisa/resultado-pesquisa.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [ListagemComponent, ProdutoDetalheComponent],
-  imports: [CommonModule, CardComponent, CategoriaRoutingModule],
+  declarations: [
+    ListagemComponent,
+    ProdutoDetalheComponent,
+    ResultadoPesquisaComponent,
+    CardComponent,
+  ],
+  imports: [CommonModule, CategoriaRoutingModule, SharedModule],
   exports: [ListagemComponent],
 })
 export class CategoriaModule {}
