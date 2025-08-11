4f2669c2125c3f8964e814a478ab309fed86078f
Ana Luiza <analuizagbarros@gmail.com>
2025-08-11 11:48:13 -0300

    Refact: Loading agora aceitando tamanhos diferentes

--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'tt-loading-spinner',
   templateUrl: './loading-spinner.component.html',
   styleUrl: './loading-spinner.component.scss',
 })
-export class LoadingSpinnerComponent {}
+export class LoadingSpinnerComponent {
+  @Input() size: 'sm' | 'md' = 'md';
+
+  get spinnerClasses(): Record<string, boolean> {
+    return {
+      spinner: true,
+      'spinner-sm': this.size === 'sm',
+      'spinner-md': this.size === 'md',
+    };
+  }
+}
